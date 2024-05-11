e5a71b19dfb51db13c535be84a0706310089c6f0
art5hur <arthurms2004@gmail.com>
2024-05-10 22:43:29 -0300

    Atualização final

--- src/main/java/com/github/art5hur/mvc_rh/controller/FuncionarioController.java
+++ src/main/java/com/github/art5hur/mvc_rh/controller/FuncionarioController.java
@@ -1,14 +1,12 @@
 package com.github.art5hur.mvc_rh.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.github.art5hur.mvc_rh.model.Funcionario;
 import com.github.art5hur.mvc_rh.repository.FuncionarioRepository;
@@ -37,5 +35,32 @@ public class FuncionarioController {
 	public String create(Model model) {		
 		model.addAttribute("funcionario", new Funcionario());
 		return "funcionario/form";
+	}
+	/*
+	@GetMapping("update/{id}")
+	public String update(@PathVariable Long id, Model model){
+
+		Optional<Funcionario> opt = funcionarioRepository.findById(id);
+		Funcionario funcionario = null;
+		if(opt.isPresent()){
+			funcionario = opt.get();
+		}
+
+		model.addAttribute("funcionario", funcionario);
+		return "funcionario/form";
+	}
+	*/
+	@GetMapping("update/{id}")
+	public String update(@PathVariable Long id, Model model){
+		Funcionario funcionario = funcionarioRepository.findById(id).orElse(new Funcionario());
+
+		model.addAttribute("funcionario", funcionario);
+		return "funcionario/form";
+	}
+
+	@GetMapping("delete/{id}")
+	public String delete(@PathVariable Long id){
+		funcionarioRepository.deleteById(id);
+		return "redirect:/funcionarios";
 	}
 }
