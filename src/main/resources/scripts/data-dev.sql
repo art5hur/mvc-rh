8315cd446905789d975f3ba1ea31a054172e1d1b
art5hur <arthurms2004@gmail.com>
2024-05-17 20:57:40 -0300

    Departamento repository

---
+++ src/main/resources/scripts/data-dev.sql
@@ -0,0 +1,18 @@
+insert into funcionarios (id, nome_funcionario, data_entrada) values
+(nextval('funcionarios_seq'), 'Pelé', '1950-12-31');
+
+insert into funcionarios (id, nome_funcionario, data_entrada) values
+(nextval('funcionarios_seq'), 'Arthur', '2000-12-31');
+
+insert into funcionarios (id, nome_funcionario, data_entrada) values
+(nextval('funcionarios_seq'), 'João', '2022-12-31');
+
+insert into funcionarios (id, nome_funcionario, data_entrada) values
+(nextval('funcionarios_seq'), 'João', '2022-12-31');
+
+
+insert into departamentos (id, nome) values
+(nextval('departamentos_seq'), 'RH');
+
+insert into departamentos (id, nome) values
+(nextval('departamentos_seq'), 'Funcionario');
